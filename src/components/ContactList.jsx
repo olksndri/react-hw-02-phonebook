7833e01cf1e624efbb7ee76ad2e7d024d48358c8
Oleksandr Tympshenko <hermeusmanua@gmail.com>
2023-07-08 21:08:24 +0300

    hw done

---
+++ src/components/ContactList.jsx
@@ -0,0 +1,27 @@
+import css from '../styles/app.module.css'
+
+export const ContactList = ({ contacts, filter, onDelete }) => { 
+    return (
+        <>
+            <ul className={css['contacts-list']}>
+                {(filter === '') ?
+                contacts.map(el => {
+                        return (
+                            <li key={el.id} className={css['contacts-list-item']}>
+                            <span>{el.name}: {el.number}</span>
+                            <button type="button" onClick={onDelete} key={el.id}>Delete</button>
+                            </li>) 
+                }) : 
+                contacts.map(el => {
+                    if (el.name.toLowerCase().includes(filter.toLowerCase())) {
+                        return (
+                            <li key={el.id} className={css['contacts-list-item']}>
+                            <span>{el.name}: {el.number}</span>
+                            <button type="button" onClick={onDelete} key={el.id}>Delete</button>
+                            </li>)
+                    }
+                })}
+            </ul>
+        </>
+    )
+}
---
+++ src/components/Filter.jsx
@@ -0,0 +1,10 @@
+import css from '../styles/app.module.css'
+
+export const Filter = ({ onInput, filterId, filter }) => { 
+    return ( 
+        <>
+            <label htmlFor={filterId}>Search contacts by name</label>
+            <input type="text" id={filterId} onInput={onInput} value={filter}/>
+        </>
+    )
+}
